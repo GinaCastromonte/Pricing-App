04758569a8d581fba37a31662a9e699fbc514c75
GinaCastromonte <123599985+GinaCastromonte@users.noreply.github.com>
2025-02-03 21:23:10 -0500

    connected tailwind

---
+++ src/data/pricingData.js
@@ -0,0 +1,27 @@
+
+
+const pricingData = [
+  {
+    title: "Basic",
+    price: "$19.99",
+    storage: "500 GB Storage",
+    users: "2 Users Allowed",
+    send: "Send up to 3 GB",
+  },
+  {
+    title: "Professional",
+    price: "$24.99",
+    storage: "1 TB Storage",
+    users: "5 Users Allowed",
+    send: "Send up to 10 GB",
+  },
+  {
+    title: "Master",
+    price: "$39.99",
+    storage: "2 TB Storage",
+    users: "10 Users Allowed",
+    send: "Send up to 20 GB",
+  },
+];
+
+export default pricingData;
